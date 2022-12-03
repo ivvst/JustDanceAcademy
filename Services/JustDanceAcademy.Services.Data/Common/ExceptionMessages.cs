396266b9ba8bae03d8efde9372399167e6e3fa61
Vania <Vania@DESKTOP-20TMCGL>
2022-12-03 22:23:44 +0200

    Make error simplify and clean

--- Services/JustDanceAcademy.Services.Data/Common/ExceptionMessages.cs
+++ Services/JustDanceAcademy.Services.Data/Common/ExceptionMessages.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace JustDanceAcademy.Services.Data.Common
 {
-    public  class ExceptionMessages
+    public class ExceptionMessages
 
     {
-        public const string ReviewNotAllowed = "Review for movie with id {0} is not allowed.";
+        public const string ReviewNotAllowed = "Review for danceClass is not allowed.";
 
-        public const string StudentNotFound = "Student with id {0} is not found.";
+        public const string StudentNotFound = "Student is not found.";
 
         public const string ClassDanceNotFound = "Dance-Class  is not found.";
 
-        public const string InstructorNotFound = "Instructor with id {0} is not found.";
+        public const string InstructorNotFound = "Instructor is not found.";
 
-        public const string InvalidDanceCategoryType = "Dance category type {0} is invalid.";
+        public const string InvalidDanceCategoryType = "Dance category is invalid.";
 
-        public const string InstructorAlreadyExists = "Instructor with name {0} already exists";
+        public const string InstructorAlreadyExists = "Instructor already exists";
 
-        public const string AdminHaveNotClass = "Admin with {0} can not have StudentLisence ";
+        public const string AdminHaveNotClass = "Admin can not have StudentLisence ";
 
 
-        public const string ClassAlreadyIsStarted = "Class with id {0} is not added because you already start a class.";
+        public const string ClassAlreadyIsStarted = " The Class is not added because you already start a class.";
     }
 }
