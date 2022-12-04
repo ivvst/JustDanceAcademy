1fb970f3792dfb2813698dbb8a886f88da3a2e9b
Vania <Vania@DESKTOP-20TMCGL>
2022-12-04 18:24:06 +0200

    Remove Unused lines

--- Web/JustDanceAcademy.Web/Controllers/ClassController.cs
+++ Web/JustDanceAcademy.Web/Controllers/ClassController.cs
@@ -4,8 +4,6 @@
 	using System.Linq;
 	using System.Security.Claims;
 	using System.Threading.Tasks;
-	using JustDanceAcademy.Data.Models;
-	using JustDanceAcademy.Models;
 	using JustDanceAcademy.Services.Data.Common;
 	using JustDanceAcademy.Services.Data.Constants;
 	using JustDanceAcademy.Web.ViewModels.Models;
