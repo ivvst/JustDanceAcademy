b7724e64df560a6768e8f4f1a9aea6a1151b4473
Vania <Vania@DESKTOP-20TMCGL>
2022-12-05 22:49:02 +0200

    Add messages

--- Services/JustDanceAcademy.Services.Data/Common/ExceptionMessages.cs
+++ Services/JustDanceAcademy.Services.Data/Common/ExceptionMessages.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JustDanceAcademy.Services.Data.Common
+﻿namespace JustDanceAcademy.Services.Data.Common
 {
-    public class ExceptionMessages
+	public class ExceptionMessages
 
-    {
-        public const string ReviewNotAllowed = "Review for danceClass is not allowed.";
+	{
+		public const string ReviewNotAllowed = "Review for danceClass is not allowed.";
 
-        public const string StudentNotFound = "Student is not found.";
+		public const string StudentNotFound = "Student is not found.";
 
-        public const string ClassDanceNotFound = "Dance-Class  is not found.";
+		public const string ClassDanceNotFound = "Dance-Class  is not found.";
 
-        public const string InstructorNotFound = "Instructor is not found.";
+		public const string InstructorNotFound = "Instructor is not found.";
 
-        public const string InvalidDanceCategoryType = "Dance category is invalid.";
+		public const string InvalidDanceCategoryType = "Dance category is invalid.";
 
-        public const string InstructorAlreadyExists = "Instructor already exists";
+		public const string InstructorAlreadyExists = "Instructor already exists";
 
-        public const string AdminHaveNotClass = "Admin can not have StudentLisence ";
+		public const string AdminHaveNotClass = "Admin can not have StudentLisence ";
+
+		public const string UserAlreadyPaid = "You already pay for your Train-Class";
+
+		public const string StartClass = "Welcome to your Class";
 
 
-        public const string ClassAlreadyIsStarted = " The Class is not added because you already start a class.";
-    }
+
+		public const string ClassAlreadyIsStarted = " The Class is not added because you already start a class.";
+	}
 }
