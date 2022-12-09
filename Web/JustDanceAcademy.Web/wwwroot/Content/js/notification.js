c812d56caf81b3c9af7492532b731cffed082746
Vania <Vania@DESKTOP-20TMCGL>
2022-12-09 11:04:01 +0200

    Add SignalR

---
+++ Web/JustDanceAcademy.Web/wwwroot/Content/js/notification.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/notificationHub").build();
+
+connection.start();
+
+connection.on("ReceiveMsg", function (msg) {
+
+    var li = document.createElement("li");
+    li.textContent = msg;
+   /* document.getElementById("msglist").appendChild(li);*/
+    setTimeout(function () {
+        toastr.options = {
+            closeButton: true,
+            progressBar: true,
+            showMethod: 'slideDown',
+            timeOut: 4000
+        };
+        toastr.info(msg);
+
+    }, 1300);
+})
+
