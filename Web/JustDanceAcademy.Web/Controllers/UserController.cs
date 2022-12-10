15631497a2692cee64b111b9de2930d8bca2c104
Vania <Vania@DESKTOP-20TMCGL>
2022-12-10 18:30:41 +0200

    Add local-storage for dark theme

--- Web/JustDanceAcademy.Web/Controllers/UserController.cs
+++ Web/JustDanceAcademy.Web/Controllers/UserController.cs
@@ -1,8 +1,15 @@
 ﻿namespace JustDanceAcademy.Controllers
 {
+	using JustDanceAcademy.Models;
 	using Microsoft.AspNetCore.Mvc;
 
 	public class UserController : Controller
 	{
+
+		public IActionResult About()
+		{
+			var model = new HomeViewModel();
+			return this.View(model);
+		}
 	}
 }
